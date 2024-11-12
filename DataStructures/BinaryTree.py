e4fcb3a13fd719e6b263fe0a967fdd1a957c2880
lloydagola <lloydagola@users.noreply.github.com>
2024-11-12 23:02:27 +0300

    implemented basic binary tree

---
+++ DataStructures/BinaryTree.py
@@ -0,0 +1,42 @@
+class TreeNode :
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        
+
+def traverseNodes(node):
+    if node is None:
+        return
+    
+    print(node.data)
+    
+    traverseNodes(node.left)
+    traverseNodes(node.right)
+
+
+#set up nodes
+R_TreeNode = TreeNode("R")
+A_TreeNode = TreeNode("A")
+B_TreeNode = TreeNode("B")
+c_TreeNode = TreeNode("c")
+D_TreeNode = TreeNode("D")
+E_TreeNode = TreeNode("E")
+F_TreeNode = TreeNode("F")
+G_TreeNode = TreeNode("G")
+
+#link nodes
+R_TreeNode.left = A_TreeNode
+R_TreeNode.right = B_TreeNode
+
+A_TreeNode.left = c_TreeNode
+A_TreeNode.right = D_TreeNode
+
+B_TreeNode.left = E_TreeNode
+B_TreeNode.right = F_TreeNode
+
+F_TreeNode.left = G_TreeNode
+
+traverseNodes(R_TreeNode)
+
+
