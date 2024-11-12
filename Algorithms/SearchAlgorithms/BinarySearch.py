c15c20255a19addbc560e079cea8113c0fa99e35
lloydagola <lloydagola@users.noreply.github.com>
2024-11-12 23:02:43 +0300

    refactoring

--- Algorithms/SearchAlgorithms/BinarySearch.py
+++ Algorithms/SearchAlgorithms/BinarySearch.py
@@ -4,27 +4,24 @@ Time Complexity: O(log n)
 Space Complexity: O(1)
 '''
 
-def binarySearch(ls, low, high, x):
+def binarySearch(ls, low, high, query):
     print("binarySearch")
     while low <= high:
         mid = low + (high-low) // 2
-
-        if x == ls[mid]:
-            return mid
-        
-        elif x < ls[mid]:
+        if query == ls[mid]:
+            return mid        
+        elif query < ls[mid]:
             high = mid - 1
-
         else:
             low = mid + 1        
     return -1
 
 
 arr = [2, 3, 4, 10, 40]
-x = 10
+query = 10
 
 # Function call
-result = binarySearch(arr, 0, len(arr)-1, x)
+result = binarySearch(arr, 0, len(arr)-1, query)
 if result != -1:
     print("Element is present at index", result)
 else:
