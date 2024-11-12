0baab3c1ce57cd8aef8d9b5646215614d23be0d5
lloydagola <lloydagola@users.noreply.github.com>
2024-11-12 23:13:22 +0300

    refactoring

--- DataStructures/BinaryTree.py
+++ DataStructures/BinaryTree.py
@@ -5,31 +5,33 @@ class TreeNode :
         self.right = None
         
 
-def traverse_nodes(node):
+def pre_order_traversal(node):
     if node is None:
         return
     
     print(node.data)
     
-    traverseNodes(node.left)
-    traverseNodes(node.right)
+    pre_order_traversal(node.left)
+    pre_order_traversal(node.right)
 
 
 #set up nodes
 R_TreeNode = TreeNode("R")
 A_TreeNode = TreeNode("A")
 B_TreeNode = TreeNode("B")
-C_TreeNode = TreeNode("c")
+C_TreeNode = TreeNode("C")
 D_TreeNode = TreeNode("D")
 E_TreeNode = TreeNode("E")
 F_TreeNode = TreeNode("F")
 G_TreeNode = TreeNode("G")
 
+
+
 #link nodes
 R_TreeNode.left = A_TreeNode
 R_TreeNode.right = B_TreeNode
 
-A_TreeNode.left = c_TreeNode
+A_TreeNode.left = C_TreeNode
 A_TreeNode.right = D_TreeNode
 
 B_TreeNode.left = E_TreeNode
@@ -37,6 +39,6 @@ B_TreeNode.right = F_TreeNode
 
 F_TreeNode.left = G_TreeNode
 
-traverse_nodes(R_TreeNode)
+pre_order_traversal(R_TreeNode)
 
 
